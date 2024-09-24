b81fd04a8e6ff3b03e23675dc680b368bc5b2573
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-09-24 03:57:39 -0300

    js inserido nos botoes

--- menu.js
+++ menu.js
@@ -5,6 +5,10 @@ const botoes = document.querySelectorAll('div.item')
 
 botoes.forEach(function(botao){
     botao.addEventListener('click', function(){
+        botao.style.scale = '1.1'
+        botao.style.background = 'lightgoldenrodyellow'
+        botao.style.fontSize = '1.5em'
+        botao.style.boxShadow = '0px 0px 15px white'
         efeito.play()
     })
 
