a56f5f994633a5fcd4db12ec9101dcb4286f17de
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-09-24 08:51:35 -0300

    pagina café atualizada

--- menu.js
+++ menu.js
@@ -1,24 +1,39 @@
 var efeito = document.getElementById('efeito')
 const botoes = document.querySelectorAll('div.item')
+let botaoClicado = null
 
 
 
 botoes.forEach(function(botao){
     botao.addEventListener('click', function(){
-        botao.style.scale = '1.1'
+        botao.style.transform = 'scale(1.1)'
         botao.style.background = 'lightgoldenrodyellow'
         botao.style.fontSize = '1.5em'
         botao.style.boxShadow = '0px 0px 15px white'
         efeito.play()
+
+        botaoClicado = botao
     })
+})
 
 efeito.addEventListener('ended', function(){
     botoes.forEach(function(botao){
-        if (botao.id == 'b1'){
+        if (botaoClicado.id === 'b1'){
             window.location.href = 'paginas/cafe.html'
+        }else if(botaoClicado.id === 'b2'){
+            window.location.href = 'https://www.google.com.br/?hl=pt-BR'
         }
     })
     
 })
-})
+
+function ver(book, menu){
+    if (menu.style.display == 'block'){
+        menu.style.display = 'none'
+    }else{
+        menu.style.display ='block'
+        book.style.transform = 'scale'
+    }
+}
+
 
